041cd235c7d0ee02242e77481404904f9ebc406e
Hans Petter Langtangen <hpl@simula.no>
2012-03-31 02:59:24 +0200

    fix

---
+++ doc/src/make.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+doconce format sphinx wrap_bioinf
+doconce sphinx_dir author="H. P. Langtangen and G. K. Sandve" title="Illustrating Python via Examples from Bioinformatics" version=0.1 theme=pyramid wrap_bioinf
+
