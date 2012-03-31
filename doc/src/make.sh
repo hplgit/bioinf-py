01d3afff2e67122982ac3364a2e0f268d58d2dfd
Hans Petter Langtangen <hpl@simula.no>
2012-03-31 08:01:13 +0200

    fix

--- doc/src/make.sh
+++ doc/src/make.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+rm -rf _static
+mkdir _static
+
+# Generate pygments versions of Python files
+cd src-bioinf
+pyg="pygmentize -f html -O full,style=emacs"
+for file in *.py; do
+  $pyg -o ../_static/$file.html -l python $file
+done
+cd ..
+
 doconce format sphinx wrap_bioinf
 doconce sphinx_dir author="H. P. Langtangen and G. K. Sandve" title="Illustrating Python via Examples from Bioinformatics" version=0.1 theme=pyramid wrap_bioinf
 
