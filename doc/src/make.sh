03bfa08dd77fe1a30e7265959c5c898761b72300
Hans Petter Langtangen <hpl@simula.no>
2012-03-31 08:39:50 +0200

    fix

--- doc/src/make.sh
+++ doc/src/make.sh
@@ -1,16 +1,35 @@
 #!/bin/sh
 
-rm -rf _static
-mkdir _static
+main=wrap_bioinf
+
+doconce format html $main
+
+static=static-bioinf
+rm -rf $static
+mkdir $static
 
 # Generate pygments versions of Python files
 cd src-bioinf
 pyg="pygmentize -f html -O full,style=emacs"
 for file in *.py; do
-  $pyg -o ../_static/$file.html -l python $file
+  $pyg -o ../$static/$file.html -l python $file
 done
+cp *.py ../$static
 cd ..
 
-doconce format sphinx wrap_bioinf
-doconce sphinx_dir author="H. P. Langtangen and G. K. Sandve" title="Illustrating Python via Examples from Bioinformatics" version=0.1 theme=pyramid wrap_bioinf
+doconce format sphinx bioinf PRIMER_BOOK=False EBOOK=False --skip_inline_comments $main
+doconce sphinx_dir author="H. P. Langtangen and G. K. Sandve" title="Illustrating Python via Examples from Bioinformatics" version=0.1 theme=pyramid $main
+python automake-sphinx.py
+
+doconce format pdflatex bioinf PRIMER_BOOK=False EBOOK=False --skip_inline_comments $main
+ptex2tex -DMINTED $main
+pdflatex $main
+pdflatex $main
+
+# Move
+cp -r sphinx-rootdir/_build/html ../tutorial/
+mv $main.pdf ../tutorial/bioinf-py.pdf
+mv $main.html ../tutorial/bioinf-py.html
+
+
 
