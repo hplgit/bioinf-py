39866202248baca61b8c911dceb77083c14637a0
Hans Petter Langtangen <hpl@simula.no>
2015-03-04 04:00:01 -0800

    updates

--- doc/src/src-bioinf/count.py
+++ doc/src/src-bioinf/count.py
@@ -127,9 +127,13 @@ def compare_efficiency():
 
 def test_count_all():
     dna = 'ATTTGCGGTCCAAA'
-    exact = dna.count('A')
+    expected = dna.count('A')
+
+    functions = [count_v1, count_v2, count_v3, count_v4,
+                 count_v5, count_v6, count_v7, count_v8,
+                 count_v9, count_v10, count_v11, count_v12]
     for f in functions:
-        success = f(dna, 'A') == exact
+        success = f(dna, 'A') == expected
         msg = '%s failed' % f.__name__
         assert success, msg
 
@@ -139,4 +143,3 @@ if __name__ == '__main__':
         test_count_all()
     elif len(sys.argv) >= 2 and sys.argv[1] == 'efficiency':
         compare_efficiency()
-
